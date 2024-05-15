7a133dcb2c75682807c43068c9f0606363eddea6
Taomosquito <S_Peloquin123@live.com>
2024-05-15 00:13:39 -0400

    performaing the git prep work from the right directory

--- express_server.js
+++ express_server.js
@@ -1,6 +1,17 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
+app.use(express.urlencoded({ extended: true }));
+
+const generateRandomString = function() {
+  const stAz = "abcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ*";
+  let newStr = '';
+  for (let i = 0; i < 6; i++) {
+    let random = Math.floor(Math.random() * stAz.length);
+    newStr += stAz[random];
+  }
+  return newStr;
+};
 
 app.set("view engine", "ejs");
 
@@ -13,7 +24,6 @@ app.get("/", (req, res) => {
   console.log("failure");
   res.send("Hello!");
 });
-
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
@@ -26,22 +36,27 @@ app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
 app.get("/urls/:id", (req, res) => {
   const urlDatabaseKey = req.params.id;
-  console.log(urlDatabaseKey);
   const templateVars = { id: urlDatabaseKey, longURL: urlDatabase[urlDatabaseKey] };
   res.render("urls_show", templateVars);
 });
-
-//--// app.get("/hello", (req, res) => {
-//--//   const templateVars = { greeting: "Hello World!" };
-//--//   res.render("hello_world", templateVars);
-//--// });
-
+app.get("/u/:id", (req, res) => {
+  console.log(urlDatabase[req.params.id]);
+  const longURL = urlDatabase[req.params.id];
+  res.redirect(longURL);
+});
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  const newID = generateRandomString();
+  urlDatabase[newID] = req.body.longURL;
+  res.redirect(`/urls/${newID}`);
+  // res.send('OK'); // Respond with 'Ok' (we will replace this)
+});
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
 
-
-//..//..// = required when I figure out the coding approach behind it!
-//--// = was an example
